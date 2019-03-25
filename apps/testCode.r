399020c7a0bd8ec8928f811a44aa0d5006ecd902
aadilk96 <aadilanilkumar@gmail.com>
2019-03-25 16:01:03 +0100

    graph depending on radio button input

--- apps/testCode.r
+++ apps/testCode.r
@@ -4,14 +4,29 @@ library(shiny)
 ## User interface with slider (input) and plot (output)
 ui <- fluidPage(
     sliderInput("n", "Number of samples", 1, 100, 10),
-    plotOutput("hist")
+    plotOutput("hist"),
+    radioButtons("dist", "Distribution type:",
+               c("Normal" = "norm",
+                 "Uniform" = "unif",
+                 "Exponential" = "exp")),
+    plotOutput("distPlot")
 )
 ## Server function connecting input and output
 server <- function(input, output){
     output$hist <- renderPlot({
-        x <- rnorm(input$n)
-        # draw n random values
-        hist(x)
+        if(input$dist == "norm"){
+            x <- rnorm(input$n)
+            # draw n random values
+            hist(x)
+            }
+        else if (input$dist == "unif"){
+            x <- runif(input$n)
+            hist(x)
+            }   
+        else if (input$dist == "exp"){
+            x <- rexp(input$n)
+            hist(x)
+            }
         })
     }
 shinyApp(ui = ui, server = server)
